5e17f2f04b6768f27039248d8710a8cd1a58a374
Impcamper <impcamper@gmail.com>
2023-04-18 21:04:15 -0600

    fixed update

--- scripts/lastupdate.js
+++ scripts/lastupdate.js
@@ -1,3 +1,3 @@
 var item = document.getElementById("update");
 const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-item.innerHTML=`Last updated ${new Date().toLocaleDateString(options)}`;
+item.innerHTML=`Last updated ${new Date(document.lastModified).toLocaleDateString(options)}`;
