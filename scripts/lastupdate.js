a285904459ed76e12f614aa35664e24cadc83a11
Impcamper <impcamper@gmail.com>
2023-05-02 21:37:24 -0600

    html is well programged (not)

--- scripts/lastupdate.js
+++ scripts/lastupdate.js
@@ -1,3 +1,5 @@
-var item = document.getElementById("update");
+let item = document.getElementById("update");
 const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
 item.innerHTML=`Last updated ${new Date(document.lastModified).toLocaleDateString(options)}`;
+let copyright=document.getElementById("year");
+copyright.innerHTML=new Date(document.lastModified).year;
