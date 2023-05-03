253b2601b939e5deb729e8096c36aa865267c5a2
Impcamper <impcamper@gmail.com>
2023-05-02 21:47:51 -0600

    reverting sugested cahnges as the broke it all

--- scripts/lastupdate.js
+++ scripts/lastupdate.js
@@ -1,5 +1,6 @@
 let item = document.getElementById("update");
 const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-item.innerHTML=`Last updated ${new Date(document.lastModified).toLocaleDateString(options)}`;
+let update=new Date(document.lastModified);
+item.innerHTML=`Last updated ${update.toLocaleDateString(options)}`;
 let copyright=document.getElementById("year");
-copyright.innerHTML=new Date(document.lastModified).year;
+copyright.innerHTML=update.getFullYear();
