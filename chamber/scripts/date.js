80ed9d0b4a2a806e25392a8c426d341c190fe4fd
Impcamper <impcamper@gmail.com>
2023-05-20 23:27:19 -0600

    Everything should be good execpt hero

--- chamber/scripts/date.js
+++ chamber/scripts/date.js
@@ -3,3 +3,6 @@ const date = new Date();
 const datefield = document.getElementById("date");
 const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(date);
 datefield.innerHTML = `<em>${fulldate}</em>`;
+if (date.getDay()==1||date.getDay()==2){
+    document.getElementById("meet").style.display="block";
+}
