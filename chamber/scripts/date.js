d46a393d3df1bf723186652de28647896a0893d0
Impcamper <impcamper@gmail.com>
2023-06-03 23:07:29 -0600

    discover page stuff

--- chamber/scripts/date.js
+++ chamber/scripts/date.js
@@ -6,3 +6,15 @@ datefield.innerHTML = `<em>${fulldate}</em>`;
 if (date.getDay()==1||date.getDay()==2){
     document.getElementById("meet").style.display="block";
 }
+
+const local= document.getElementById("lastvisit");
+if (local!=null){
+    let lastvist = window.localStorage.getItem("visit");
+    if (lastvist==null){
+        local.textContent=`This is your first visit`;
+    }
+    else{
+        local.textContent=`You last visited ${lastvist}`;
+    }
+    localStorage.setItem("vist",fulldate);
+}
