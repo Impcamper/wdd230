a768178941333d02a0f5a28ff6df77a6a409accd
Impcamper <impcamper@gmail.com>
2023-07-19 22:45:40 -0600

    90% done

--- bountyfoods/scripts/fresh.js
+++ bountyfoods/scripts/fresh.js
@@ -1,1 +1,27 @@
 const url ="https://brotherblazzard.github.io/canvas-content/fruit.json";
+
+async function getdata(){
+    const response = await fetch(url);
+    const data = await response.json();
+    displayfruit(data);
+}
+const displayfruit=(data) =>{
+    const menues=document.querySelectorAll(".fruit");
+    menues.forEach(option =>{
+        data.forEach((fruit) => {
+            let item =document.createElement("option")
+            item.value=fruit.name;
+            item.textContent=fruit.name;
+            option.appendChild(item);
+            
+        });
+    });
+}
+getdata();
+const button=document.getElementById("submit");
+button.onclick = addinfo()
+function addinfo(){
+    const holder =document.getElementById("info");
+    holder.display="block";
+    holder.innerText="lorem"
+}
